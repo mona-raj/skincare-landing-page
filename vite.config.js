c23a047809e5a89d9f8e9ac2cd7b7c64983c0db1
Mona Raj <agrmonaraj@gmail.com>
2025-06-27 23:59:15 +0530

    Fix typo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/skinacre-landing-page/",
+  base: "/skincare-landing-page/",
   plugins: [react(), tailwindcss()],
 })
